0e163cfdb858555e7fea2dd55c2b097d3ed72e3f
pjh <[je377523@gmail.com]>
2021-11-17 15:16:06 +0900

    1일차수업끝

---
+++ HelloWorld/src/hello/App.java
@@ -0,0 +1,16 @@
+package hello; //패키지 헬로우
+
+public class App { // 클래스 App 만들기
+
+	public static void main(String[] args) { // 메인 메소드. 여기에서 프로그램 실행
+		// sysout + CTRL + spacebar = 자동완성
+		System.out.println("헬로우");
+		// 실행 방법
+		// 1. 이클립스 상단 ▶ 으로 실행
+		// 2. 오른쪽 마우스 Run as 자바 애플리케이션
+		// 3. 왼쪽편 클래스파일을 선택하고 ▶ 으로 실행
+		// 단축키 : CTRL + F11
+		 
+	}
+
+}
---
+++ MainMethod/src/application/test/App.java
@@ -0,0 +1,14 @@
+package Application.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Ctrl + Alt + 아래로 = 아랫줄에 복사
+		// Tab 반대 = Shift + Tab
+		System.out.println("안녕하세요");
+		System.out.println("새 자바 프로그램을");
+		System.out.println("실행합니다.");
+
+	}
+
+}
