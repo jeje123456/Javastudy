330e792cd2e1bc688ffc77aa8a6b3aab7964d726
pjh <[je377523@gmail.com]>
2021-11-26 08:56:30 +0900

    11월25일

--- Ch07_HangmanRestart/src/application/Hangman.java
+++ Ch07_HangmanRestart/src/application/Hangman.java
@@ -45,9 +45,7 @@ public class Hangman {
 			}
 		}
 		else {
-			remainTries--;
 			System.out.println("틀렸습니다.");
-			System.out.println("남은 시도횟수 : " + remainTries);
 			if (remainTries == 0) {
 				System.out.println("Game Over!");
 				running = false;
